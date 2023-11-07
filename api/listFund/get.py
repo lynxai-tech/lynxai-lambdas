5c167db6ff07bdfb568e7e5b0533194c6c7eeeae
JohnnyMercier <116117226+JohnnyMercier@users.noreply.github.com>
2023-11-07 12:06:57 +0000

    listfunds commit

---
+++ api/listFund/get.py
@@ -0,0 +1,31 @@
+from main import *
+
+
+@lynx()
+def lambda_handler(event, context):
+    res = event.select("""
+    SELECT i.id,
+           i.date,
+           if2.id if_id,
+           i.fund_name,
+           i.fund_type,
+           i.eu_sfdr_article,
+           i.country,
+           i.status,
+           i.amount,
+           i.portfolio_weight_percentage,
+           i.client_id,
+           COUNT(id.asset_id) nr_assets
+      FROM `schema`.main_fund i
+      JOIN `schema`.client u
+        ON u.id = i.client_id
+      JOIN `schema`.investment_fund if2
+        ON if2.main_fund_id = i.id
+      LEFT JOIN `schema`.investment_detail id
+        ON id.fund_id = if2.id
+     WHERE u.name = 'lynxai'
+       AND i.date = if2.date
+     GROUP BY if2.id;
+    """).list()
+
+    return res
