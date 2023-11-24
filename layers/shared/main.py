ee6038bcda6ba9504b9caee041690aaefd738fe2
JohnnyMercier <116117226+JohnnyMercier@users.noreply.github.com>
2023-11-24 14:50:15 +0000

    fix

--- layers/shared/main.py
+++ layers/shared/main.py
@@ -6,9 +6,5 @@ from feature1 import *
 from get_fund_helpers import *
 from edit_fund_helpers import *
 from fund_dictionaries import *
-<<<<<<< HEAD
 from stock_helpers import *
-=======
 from simulator import *
-
->>>>>>> ca9c47ce385d3915b480a1cd50fb997077a81f56
